a3fa707828f178c49bfbd80e131d41b4a7c5ff87
MoRan3421 <111855367+MoRan3421@users.noreply.github.com>
2026-02-13 18:39:46 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lingyue.app',
+  appName: 'LingYueApp',
+  webDir: 'build'
+};
+
+export default config;
